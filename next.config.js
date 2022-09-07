cb63f31ac6fdd95233b31c53f5ac0df53a39dfaa
aguilarkevin <kevinaguilar0813@gmail.com>
2022-09-07 14:27:34 -0600

    update next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    dirs: ['components', 'pages', 'sections', 'theme', 'utils'],
+  },
   reactStrictMode: true,
   swcMinify: true,
 }
